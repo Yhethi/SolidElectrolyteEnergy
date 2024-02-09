8aab1ece1066eab143ac5433c85e05519f28bde4
Yhethi <eloyjosebonillag@gmail.com>
2024-02-09 14:16:07 -0400

    v1.14

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,18 +8,9 @@ import videoHome from "../assets/mp4/homepage/homepage.mp4";
 import { textos } from "./textos/Textos";
 import Pitch_Deck from "../assets/pdf/homepage/Pitch_Deck.pdf";
 import TECH from "../assets/pdf/homepage/TECH.pdf";
-
+import { Document, Page } from "react-pdf";
 
 export const Homepage = ({ PlaySound }) => {
-
-  const mousemove = (event) => {
-    const backVideo = document.querySelector(".backVideo");
-    const totalX = ((backVideo.clientWidth - event.pageX) * 0.2) / 50;
-    const totalY = ((backVideo.clientHeight - event.pageY) * -1) / 50;
-    backVideo.style.transform = `translate(${totalX}px,${totalY}px)`;
-  };
-
-  // window.addEventListener("mousemove", mousemove);
 
   return (
     <div className="body__global">
@@ -41,7 +32,10 @@ export const Homepage = ({ PlaySound }) => {
       {/* <div className="body__wallpaper"></div> */}
       <div className="body__components">
         <div className="lettersAnimation">
-          <section className="sectionHuman" data-identity="Solid Electrolyte Energy Now">
+          <section
+            className="sectionHuman"
+            data-identity="Solid Electrolyte Energy Now"
+          >
             <h1>
               <span style={{ whiteSpace: "nowrap" }}>SOLID ELECTROLYTE</span>
               <span style={{ whiteSpace: "nowrap" }}>FOR BATERIES</span>
@@ -70,11 +64,25 @@ export const Homepage = ({ PlaySound }) => {
         {/* <Hero /> */}
       </div>
       <div className="body__pitchAndTech">
-          <div className="body__pitchAndTechButtons">
-            <a className="body__download__btn logoFont" href={Pitch_Deck} download="Pitch Deck" target="_blank">Pitch Deck</a>
-            <a className="body__download__btn logoFont" href={TECH} download="TECH" target="_blank">Tech</a>
-          </div>
+        <div className="body__pitchAndTechButtons">
+          <button
+            className="body__download__btn logoFont"
+            onClick={() => {
+              window.open(Pitch_Deck);
+            }}
+          >
+            Pitch Deck
+          </button>
+          <button
+            className="body__download__btn logoFont"
+            onClick={() => {
+              window.open(TECH);
+            }}
+          >
+            The Tech
+          </button>
         </div>
+      </div>
     </div>
   );
 };
