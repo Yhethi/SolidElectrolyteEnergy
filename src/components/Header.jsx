84052290861eb0c2564679cbea845b581a027ab4
Yhethi <eloyjosebonillag@gmail.com>
2024-02-09 17:19:05 -0400

    v1.16

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -172,32 +172,34 @@ export const Header = ({ audio, section }) => {
           />
         </Link>
       </div> */}
-      <nav className="navbar navbar-expand-lg navbar-dark header__menu">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link logoFont"
-                onClick={() => {
-                  onHandleOptionConnect();
-                  setLoader(true);
-                }}
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+      <nav class="navbar navbar-expand-lg navbar-dark header__menu">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link logoFont"
+                  onClick={() => {
+                    onHandleOptionConnect();
+                    setLoader(true);
+                  }}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
